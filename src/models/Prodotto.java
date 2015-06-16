45bbc3f0cafe7abc7be4abe9e854db65d8e05737
alessio <alexma93@hotmail.it>
2015-06-16 19:42:51 +0200

    quasi niente

--- src/models/Prodotto.java
+++ src/models/Prodotto.java
@@ -2,7 +2,6 @@ package models;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
 
 
@@ -10,16 +9,13 @@ import java.util.List;
 
 
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
